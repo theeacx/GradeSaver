682750b3059e043eda4df48c1cef2cb0d88df5bb
theeacx <licxandruteodora@gmail.com>
2024-03-27 00:49:46 -0700

    interfata profil personal

---
+++ app/src/main/java/com/example/gradesaver/PersonalProfileActivity.kt
@@ -0,0 +1,11 @@
+package com.example.gradesaver
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class PersonalProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_personal_profile)
+    }
+}
