3008b9e10d8ec8f4270c3dd01657ea1af70fac93
theeacx <licxandruteodora@gmail.com>
2024-03-27 00:53:44 -0700

    adaugare interfete main studenti si profesori

---
+++ app/src/main/java/com/example/gradesaver/ProfessorMainScreenActivity.kt
@@ -0,0 +1,11 @@
+package com.example.gradesaver
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ProfessorMainScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_professor_main_screen)
+    }
+}
---
+++ app/src/main/java/com/example/gradesaver/StudentMainScreenActivity.kt
@@ -0,0 +1,11 @@
+package com.example.gradesaver
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class StudentMainScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_student_main_screen)
+    }
+}
