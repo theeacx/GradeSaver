a81b1300d3a4bbd29761cb6d90b84a5a044f8e8a
theeacx <licxandruteodora@gmail.com>
2024-07-13 23:28:26 -0700

    random little updates

--- app/src/main/java/com/example/gradesaver/adapters/ChartPageAdapter.kt
+++ app/src/main/java/com/example/gradesaver/adapters/ChartPageAdapter.kt
@@ -6,11 +6,10 @@ import com.example.gradesaver.fragments.BarChartFragment
 import com.example.gradesaver.fragments.HorizontalBarChartFragment
 import com.example.gradesaver.fragments.LineChartFragment
 import com.example.gradesaver.fragments.PieChartFragment
-import com.example.gradesaver.fragments.RadarChartFragment
 
 class ChartPagerAdapter(activity: FragmentActivity, private val userId: Int) : FragmentStateAdapter(activity) {
     override fun getItemCount(): Int {
-        return 5 // Number of chart fragments
+        return 4 // Number of chart fragments, skipping the RadarChartFragment
     }
 
     override fun createFragment(position: Int): androidx.fragment.app.Fragment {
@@ -18,8 +17,7 @@ class ChartPagerAdapter(activity: FragmentActivity, private val userId: Int) : F
             0 -> BarChartFragment.newInstance(userId)
             1 -> LineChartFragment.newInstance(userId)
             2 -> PieChartFragment.newInstance(userId)
-            3 -> RadarChartFragment.newInstance(userId)
-            4 -> HorizontalBarChartFragment.newInstance(userId)
+            3 -> HorizontalBarChartFragment.newInstance(userId)
             else -> throw IllegalStateException("Unexpected position $position")
         }
     }
--- app/src/main/java/com/example/gradesaver/fragments/BarChartFragment.kt
+++ app/src/main/java/com/example/gradesaver/fragments/BarChartFragment.kt
@@ -4,20 +4,25 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import com.example.gradesaver.R
 import com.example.gradesaver.dataClasses.EnrollmentCountByCourse
 import com.example.gradesaver.database.AppDatabase
 import com.github.mikephil.charting.charts.BarChart
+import com.github.mikephil.charting.components.XAxis
 import com.github.mikephil.charting.data.BarData
 import com.github.mikephil.charting.data.BarDataSet
 import com.github.mikephil.charting.data.BarEntry
+import com.github.mikephil.charting.highlight.Highlight
+import com.github.mikephil.charting.listener.OnChartValueSelectedListener
 import kotlinx.coroutines.launch
 
 class BarChartFragment : Fragment() {
     private var userId: Int? = null
     private lateinit var barChart: BarChart
+    private lateinit var labels: List<String>
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -40,18 +45,44 @@ class BarChartFragment : Fragment() {
 
     private fun updateBarChart(data: List<EnrollmentCountByCourse>) {
         val entries = ArrayList<BarEntry>()
-        val labels = ArrayList<String>()
+        labels = data.map { it.courseName }
 
         data.forEachIndexed { index, enrollmentCount ->
             entries.add(BarEntry(index.toFloat(), enrollmentCount.enrolledCount.toFloat()))
-            labels.add(enrollmentCount.courseName)
         }
 
         val dataSet = BarDataSet(entries, "No of enrollments")
         dataSet.color = resources.getColor(R.color.purple, null)
         val barData = BarData(dataSet)
+        barData.barWidth = 0.9f  // set custom bar width
         barChart.data = barData
-        barChart.invalidate()
+
+        // Setting up X-axis
+        val xAxis = barChart.xAxis
+        xAxis.setDrawLabels(false) // Disable drawing of X-axis labels
+        xAxis.position = XAxis.XAxisPosition.BOTTOM
+        xAxis.setDrawGridLines(false)
+        xAxis.granularity = 1f
+        xAxis.isGranularityEnabled = true
+
+        barChart.description.isEnabled = false
+        barChart.setFitBars(true)  // make the x-axis fit exactly all bars
+        barChart.invalidate()  // refresh
+
+        // Set up value selected listener
+        barChart.setOnChartValueSelectedListener(object : OnChartValueSelectedListener {
+            override fun onValueSelected(e: com.github.mikephil.charting.data.Entry?, h: Highlight?) {
+                e?.let {
+                    val index = it.x.toInt()
+                    if (index >= 0 && index < labels.size) {
+                        val label = labels[index]
+                        Toast.makeText(context, "Course: $label\nEnrollments: ${it.y.toInt()}", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+
+            override fun onNothingSelected() {}
+        })
     }
 
     companion object {
